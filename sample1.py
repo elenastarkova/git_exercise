b4a6e7f769b6c35f4d3bdd76ef86b2ed1d6f59cc
elenastarkova <104604766+elenastarkova@users.noreply.github.com>
2022-04-28 21:44:28 -0400

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -1,1 +1,11 @@
-#simple file for git
+# sample file for git
+print("hello World!")
+print("more lines in the sample1")
+for i in range(10):
+    print(f"numbers : {i}")
+
+# HW: Register to the github and save your email, password
+print("New changes after last push, fixed PROJ-23455")
+
+print("this line is for PROJ-23455 test failed")
+print("applied new changes to fix the failed issue")
